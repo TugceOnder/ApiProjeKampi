965ae682ef1093d8bb49df41a609ae21043435f3
TugceOnder <tugceonder775@gmail.com>
2026-09-22 23:31:31 +0300

    Feature yapıldı

---
+++ ApiProjeKampi.WEB.UI/ViewComponents/_HeadDefaultComponentPartial.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiProjeKampi.WEB.UI.ViewComponents;
+using Microsoft.AspNetCore.Mvc;
+    public class _HeadDefaultComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+
---
+++ ApiProjeKampi.WEB.UI/ViewComponents/_NavbarDefaultComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiProjeKampi.WEB.UI.ViewComponents;
+using Microsoft.AspNetCore.Mvc;
+    public class _NavbarDefaultComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
